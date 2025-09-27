fb09012ddd8744d9fe0d2b9de109b303fce5c1e1
imoT-py <imot.nagy@gmail.com>
2025-09-27 20:21:56 +0200

    Simplify main.py and create flow for low elo

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 import time
 from create_folder import folder_file
-from puuids import get_puuids_challengers
+from puuids import get_puuids
 from process import process
+from puuids import get_puuids_low
 
 # 2. Get the user_ids OK
 # 3. Get the their rank -> to user_list.txt // máshogy: adott lekérésben vagyunk, tudjuk rank, tier-t -> az alapján mentünk
@@ -10,8 +11,21 @@ from process import process
 # create outputs folder and users.txt file
 folder_file()
 
-# get the 'challengers' data
-url = "https://euw1.api.riotgames.com/lol/league/v4/challengerleagues/by-queue/RANKED_SOLO_5x5"
-puuids = get_puuids_challengers(url)
-print(len(puuids))
-process(puuids, "challenger", 1)
+rank_list = ['challenger', 'grandmaster', 'master']
+
+for rank in rank_list:
+    puuids = get_puuids(rank)
+    process(puuids, rank, 'na')
+
+low_elo = ["IRON", "BRONZE", "SILVER", "GOLD", "PLATINUM", "EMERALD", "DIAMOND"]
+tiers = ["I", "II", "III", "IV"]
+
+for rank in low_elo:
+        for tier in tiers:
+            puuids = get_puuids_low(rank, tier)
+            process(puuids, rank, tier)       
+            
+
+# hibamentes futás -> error prone try, except!
+# még egyszerűsités?
+# kimaxolni az api limiteket, a lehető leggyorsabb mentés!
--- src/puuids.py
+++ src/puuids.py
@@ -5,17 +5,35 @@ from http.client import IncompleteRead
 from requests.exceptions import ChunkedEncodingError, ConnectionError, HTTPError
 from responses import response_data
 
-def get_puuids_challengers(url):    
+def get_puuids(rank):    
     
+    url = f"https://euw1.api.riotgames.com/lol/league/v4/{rank}leagues/by-queue/RANKED_SOLO_5x5"
     response = response_data(url, None)
              
     data = response.json()
-    challengers = data['entries']
+    users = data['entries']
 
     list_puuids = []
 
-    for challenger in challengers:
-        puuid_data = challenger['puuid']
+    for user in users:
+        puuid_data = user['puuid']
         list_puuids.append(puuid_data)
             
+    return list_puuids
+
+def get_puuids_low(rank, tier):
+    
+    list_puuids = []
+    
+    for page in range(1, 51):
+        
+        url = f"https://euw1.api.riotgames.com/lol/league/v4/entries/RANKED_SOLO_5x5/{rank}/{tier}?page={page}"
+        response = response_data(url, None)
+        
+        data = response.json()
+        
+        for user in data:
+            inner_data = user['puuid']
+            list_puuids.append(inner_data)
+    
     return list_puuids
