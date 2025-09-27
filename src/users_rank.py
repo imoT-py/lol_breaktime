e620b7ea245d517a1ff301e673f2309f915f5c89
imoT-py <imot.nagy@gmail.com>
2025-09-27 15:58:41 +0200

    Create users_rank.py to save users' rank

---
+++ src/users_rank.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+def users_rank(user_id, rank, tier):
+    
+    directory_path = Path(__file__).resolve().parent.parent
+    data_path = directory_path / 'outputs'
+        
+    with open(str(data_path) + "/" + "users.txt", "a") as file:
+        file.write(user_id + ' ' + rank + ' ' + tier + '\n')
