c89bfcf664c9e355c815a64eac8cf3f78e75a6cb
Tom <126701625+imoT-py@users.noreply.github.com>
2025-10-06 15:02:17 +0200

    Update user_file.py change back to save in output folder

--- src/user_file.py
+++ src/user_file.py
@@ -6,10 +6,10 @@ def user_file(info, user_id, rank):
     directory_path = Path(__file__).resolve().parent.parent
     data_path = directory_path / 'outputs'
         
-    with open(str(data_path) + "/" + rank + "/" + user_id + ".txt", "a+") as file:
+    with open(str(data_path) + "/" + user_id + ".txt", "a+") as file:
         for data in info:
             print(str(data), end=" ", file=file)
-    with open(str(data_path) + "/" + rank + "/" + user_id + ".txt", "a+") as file:        
+    with open(str(data_path) + "/" + user_id + ".txt", "a+") as file:        
         print(file=file)
         
     print("Match saved!")
